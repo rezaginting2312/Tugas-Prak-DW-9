b49c44cea626cc6c1f9f862015333223b7928bfd
rezaginting2312 <gintingreza557@gmail.com>
2024-11-19 22:04:13 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  darkMode: 'class', 
+  theme: {
+    extend: {
+      colors: {
+        
+        primary: '#1E40AF',  
+        secondary: '#FBBF24', 
+        
+       
+        darkPrimary: '#0F172A',  
+        darkSecondary: '#EAB308', 
+        darkBg: '#1E293B',        
+        darkText: '#F8FAFC',     
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'], 
+      },
+    },
+  },
+  plugins: [],
+};
